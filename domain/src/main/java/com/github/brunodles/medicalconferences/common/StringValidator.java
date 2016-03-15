927549607b44a418430b4ab337a773a8b9604553
Bruno de Lima e Silva <dlimaun@gmail.com>
2016-03-15 12:47:03 -0300

    Make StringValidator final

--- domain/src/main/java/com/github/brunodles/medicalconferences/common/StringValidator.java
+++ domain/src/main/java/com/github/brunodles/medicalconferences/common/StringValidator.java
@@ -3,7 +3,10 @@ package com.github.brunodles.medicalconferences.common;
 /**
  * Created by bruno on 04/09/15.
  */
-public class StringValidator {
+public final class StringValidator {
+
+    private StringValidator() {
+    }
 
     public static boolean isNullOrEmpty(String string) {
         return (string == null) || (string.length() == 0);
