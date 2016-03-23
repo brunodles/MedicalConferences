95b2ddc1aed70bea6e3e6d5c536dd0ac7347aa59
Bruno de Lima e Silva <dlimaun@gmail.com>
2016-03-23 18:29:57 -0300

    Fix active record test

--- repositories_activeandroid/src/test/java/com/github/brunodles/medicalconferences/repositories/ContactRepositoryTest/WhenWantToSaveAContact.java
+++ repositories_activeandroid/src/test/java/com/github/brunodles/medicalconferences/repositories/ContactRepositoryTest/WhenWantToSaveAContact.java
@@ -4,8 +4,10 @@ import android.os.Build;
 
 import com.github.brunodles.medicalconferences.TestApplication;
 import com.github.brunodles.medicalconferences.entity.Contact;
+import com.github.brunodles.medicalconferences.entity_impl.ContactImpl;
 import com.github.brunodles.medicalconferences.repositories.BuildConfig;
 import com.github.brunodles.medicalconferences.repositories.ContactRepository;
+import com.github.brunodles.medicalconferences.repositories.dtos.ContactRecord;
 import com.github.brunodles.medicalconferences.reposytory.Listener;
 
 import org.junit.Before;
@@ -30,13 +32,13 @@ public class WhenWantToSaveAContact {
     @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     private ContactRepository repository;
-    private Contact contact;
+    private ContactRecord contact;
     @Mock Listener listener;
 
     @Before
     public void before() {
         repository = new ContactRepository();
-        contact = new Contact();
+        contact = new ContactRecord();
         contact.setName("Anakin Skywalker");
         contact.setPhone("99999-8888");
         contact.setEmail("anakin@deathstar.com");
